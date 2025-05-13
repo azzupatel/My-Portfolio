65a296db0126a49a509f667b30c5c2903d7a2b54
Abdulaziz Patel <patelazzu19@gmail.com>
2025-05-13 09:44:47 +0530

    Update profileconfig.js

--- src/profileconfig.js
+++ src/profileconfig.js
@@ -1,5 +1,5 @@
 export const name = "Abdulaziz";
-export const title = "Founder of this Patel Bro's Tech";
+export const title = "Founder of the Patel Bro's Tech";
 export const email = "patelazzu19@gmail.com";
 export const github = "https://github.com/azzupatel";
 export const linkedin = "https://www.linkedin.com/in/abdulaziz-patel-424921299";
@@ -12,11 +12,11 @@ export const instagram = "https://www.instagram.com/patel_bros_tech"
 
 export const skills = [
     "Web Developer",
+    "Web Designer",
     "Digital Marketer",
-    "Graphic Designer",
 ];
 
-export const resume = "./Resume Azzu.pdf"; //resume is working fine now
+export const resume = "./Abdulaziz_patel_resume"; //resume is working fine now
 export const contactFormsparkId = ''
 
 
